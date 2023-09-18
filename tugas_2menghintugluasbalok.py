18b31c2e1cbd824a5c844aad4b928fc9f7e084f2
0827049501 - Fahmi Syuhada <fahmisyuhadaa@gmail.com>
2023-09-18 09:55:42 +0800

    Tugas 1 Menghintung luas balok
    hari ini saya kerjakan tugas balok pak, soalnya susah sekali, dan uang bulanan sudah habis!!!!

---
+++ tugas_2menghintugluasbalok.py
@@ -0,0 +1,85 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hallo")
+hallo
+print("hai")
+hai
+
+============== RESTART: C:/Users/LA-BAGU00/Desktop/LATIHAN/test.py =============
+hallo
+selemat pagi
+
+============== RESTART: C:/Users/LA-BAGU00/Desktop/LATIHAN/test.py =============
+hallo
+selemat pagi
+apakah anda sudah sarapan??
+Belum, saya belum sarapn!!!
+
+============== RESTART: C:/Users/LA-BAGU00/Desktop/LATIHAN/test.py =============
+hallo
+selemat pagi
+apakah anda sudah sarapan??
+Belum, saya belum sarapn!!!
+1+1
+2
+2+2+23-10*12
+-93
+10*15
+150
+"test"
+'test'
+nama = "anggi"
+print(nama)
+anggi
+alamat = "KETARA"
+print(alamat)
+KETARA
+print(nama+" " + alamat)
+anggi KETARA
+"1" + "1"
+'11'
+1+1
+2
+"1" + "1"
+'11'
+print(nama)
+anggi
+print("EKA")
+EKA
+print(semester)
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    print(semester)
+NameError: name 'semester' is not defined
+semester = "1"
+print(semester)
+1
+r = 100
+luas = 3.14*r*r
+print(luas)
+31400.0
+>>> rr
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    rr
+NameError: name 'rr' is not defined. Did you mean: 'r'?
+>>> r*r
+10000
+>>> sisi=4
+>>> keliling=3*s
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    keliling=3*s
+NameError: name 's' is not defined
+>>> keliling=3*sisi
+>>> print(keliling)
+12
+>>> panjang=10
+>>> lebar=5
+>>> tinggi=12
+>>> LuasBalok=panjang*lebar*tinggi
+>>> print(LuasBalok)
+600
+>>> 
+= RESTART: C:/Users/LA-BAGU00/AppData/Local/Programs/Python/Python311/menghitungLuasBalok.py
+600
